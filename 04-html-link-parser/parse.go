1b54557cb91606e52442adc301554b247e184d52
Emilio Schepis <emilio.schepis@gmail.com>
2020-07-15 22:14:26 +0200

    complete part two of link parser

--- 04-html-link-parser/parse.go
+++ 04-html-link-parser/parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a link in an HTML document.
 type Link struct {
@@ -10,5 +15,27 @@ type Link struct {
 
 // Parse will take in an HTML document and return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	dfs(doc, "")
+
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+
+	fmt.Println(padding, msg)
+
+	// This is just a normal for loop, the third part simply instructs the loop on
+	// what to do after each iteration (select the next sibling node).
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/emilioschepis/gophercises
 
 go 1.14
 
-require gopkg.in/yaml.v2 v2.3.0
+require (
+	gopkg.in/yaml.v2 v2.3.0
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381
+)
